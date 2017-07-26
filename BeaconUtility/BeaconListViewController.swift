bf7c5986a56c7a1ccd1428833e9559f02705a115
Mike Vartanian <mike.vartanian@gmail.com>
2017-07-26 15:16:25 -0400

    Initial add of assignment 5 project proposal to temp github repository to continue work until new directory is created.

---
+++ BeaconUtility/BeaconListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BeaconListViewController.swift
+//  BeaconUtility
+//
+//  Created by Michael Vartanian on 7/18/17.
+//  Copyright © 2017 Michael Vartanian. All rights reserved.
+//
+
+import UIKit
+
+class BeaconListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
